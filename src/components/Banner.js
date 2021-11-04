4df1a67ad6671045f340d8a4af3a0a21be7912ec
mounia fattouh <mounia.fattouh@gmail.com>
2021-11-04 21:48:11 +0100

    Add Banner component + style

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+class Banner extends Component {
+  render() {
+    return (
+      <div className="banner">
+        <h1 className="banner_title">Chez vous, partout et ailleurs</h1>
+      </div>
+    )
+  }
+}
+
+export default Banner
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import { Component } from 'react'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
 
 class Home extends Component {
   render() {
     return (
       <div>
         <Header />
+        <main>
+          <div className="container">
+            <Banner />
+          </div>
+        </main>
       </div>
     )
   }
