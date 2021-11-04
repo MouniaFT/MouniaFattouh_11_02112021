fdb7146fcf360231f29ff722c97f44dde21eb26b
mounia fattouh <mounia.fattouh@gmail.com>
2021-11-04 20:34:45 +0100

    Modification in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,17 @@ class Header extends Component {
   render() {
     return (
       <header className="header">
-        <div className="logo">
-          <img src={logo} alt="logo" />
+        <div className="container">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <nav className="nav">
+            <ul className="nav_list">
+              <li className="nav_link active">Accueil</li>
+              <li className="nav_link">A Propos</li>
+            </ul>
+          </nav>
         </div>
-        <nav className="nav">
-          <ul className="nav_list">
-            <li className="nav_link active">Accueil</li>
-            <li className="nav_link">A Propos</li>
-          </ul>
-        </nav>
       </header>
     )
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 class Home extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <Header />
       </div>
     )
