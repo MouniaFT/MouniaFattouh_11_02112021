ded2b0e65b63979349374156bebcfe9594ad123e
mounia fattouh <mounia.fattouh@gmail.com>
2021-11-04 22:34:51 +0100

    fetch data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,28 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch(
+      'https://raw.githubusercontent.com/MouniaFT/MouniaFattouh_11_02112021/main/public/json/data.json'
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          data: result,
+        })
+      })
+  }
+
   render() {
+    const { data } = this.state
+
     return (
       <div>
         <Header />
